f6b031e4d43d4e7ca99932789503e447f2edc8c7
Parzifal <plclavijop@gmail.com>
2023-12-13 12:15:56 -0500

    Pokedex homepage
    - Add new Home page component.
    - Add react-router-dom and sass as dependencies.
    - Add pokeball css background figure.

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import './Home.scss'
+
+export default function Home() {
+  return (
+    <div className='home'>
+      <div className="bg-pokeball"></div>
+      <div>Home</div>
+    </div>
+  )
+}
